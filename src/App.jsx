bf94ad950e9398ad7d494c6e7b748019c0fdedb5
iniix <iniix@proton.me>
2026-07-06 13:27:31 -0400

    whooooole bunchastuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import "./App.css";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Loader from "./components/Loader";
 
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
+import Guestbook from "./pages/Guestbook";
+import Gallery from "./pages/Gallery";
 
 export default function App() {
+
+    const [loading, setLoading] = useState(true);
+
+    if (loading) {
+        return <Loader onFinish={() => setLoading(false)} />;
+    }
 
     return (
         <>
             <Navbar />
 
             <Routes>
-
                 <Route path="/" element={<Home />} />
-
                 <Route path="/blog" element={<Blog />} />
-
+                <Route path="/guestbook" element={<Guestbook />} />
+                <Route path="/gallery" element={<Gallery />} />
             </Routes>
 
             <Footer />
