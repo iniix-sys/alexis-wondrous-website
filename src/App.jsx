a9c7d24889b77b3a727cd727280325b2117df566
iniix <iniix@proton.me>
2026-07-07 14:34:17 -0400

    mobile support

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -7,8 +8,23 @@ import Guestbook from "./pages/Guestbook";
 
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import Loader from "./components/Loader";
+
 
 export default function App() {
+
+    const [loading, setLoading] = useState(true);
+
+
+    if (loading) {
+        return (
+            <Loader
+                onFinish={() => setLoading(false)}
+            />
+        );
+    }
+
+
     return (
         <div className="app-layout">
 
@@ -17,14 +33,21 @@ export default function App() {
             <div className="main-layout">
 
                 <Sidebar />
-                
+
                 <main className="page-area">
+
                     <Routes>
+
                         <Route path="/" element={<Home />} />
+
                         <Route path="/blog" element={<Blog />} />
+
                         <Route path="/gallery" element={<Gallery />} />
+
                         <Route path="/guestbook" element={<Guestbook />} />
+
                     </Routes>
+
                 </main>
 
             </div>
